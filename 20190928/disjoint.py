ee6fed0d17616d572a03e1402987fda6b276d73b
kobayu0902art <kobayu2019art@gmail.com>
2019-10-02 16:34:32 +0900

    add

--- 20190928/disjoint.py
+++ 20190928/disjoint.py
@@ -1,23 +1,21 @@
 import sys
+import itertools
 input = sys.stdin.readline
 
 #複数入力
 a,b=(int(x) for x in input().split())
 
-def make_divisors(n):
-    divisors = []
-    for i in range(1, int(n**0.5)+1):
-        if n % i == 0:
-            divisors.append(i)
-            if i != n // i:
-                divisors.append(n//i)
+def factorize(n):
+    b = 2
+    #本当は[]
+    fct = [1]
+    while b * b <= n:
+        while n % b == 0:
+            n //= b
+            fct.append(b)
+        b = b + 1
+    if n > 1:
+        fct.append(n)
+    return fct
 
-    # divisors.sort()
-    return divisors
-
-ad=make_divisors(a)
-bd=make_divisors(b)
-
-cd=set(ad)&set(bd)
-
-print(ad,bd,cd)
+print(len((set(factorize(a))&set(factorize(b)))))
